2f05a14c6c6fd90fa60a720093e50459c823b7dc
bouhhhhh <122919005+bouhhhhh@users.noreply.github.com>
2023-01-17 14:04:22 -0500

    Update hello.py
    bonjour, j'ai modifié l'exercice

--- hello.py
+++ hello.py
@@ -1,5 +1,6 @@
 import math
 #TODO Affichez l'aire d'un cercle avec le rayon indiqué
 rayon = 4
+print(math.pi*rayon**2)
+print("yoakim Galipeau-Theberge")
 
-#TODO Ajoutez une ligne qui affichera votre nom et prénom à la fin
